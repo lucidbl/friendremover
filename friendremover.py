dc07860971fa60bf7b9d1e6c47c612dbabba70e6
mythical <95757470+namevariable@users.noreply.github.com>
2022-08-05 18:26:30 +0200

    Update friendremover.py

--- friendremover.py
+++ friendremover.py
@@ -1,7 +1,7 @@
 import requests
 import time
 
-token = {"authorization":"MTAwMDUyODM1NzIwMjU5MTg2NQ.GVDSeZ.VK39aKcvqOV2i4Qm9ZaBQP3RO7TqOdSWdBJuW4"}
+token = {"authorization":"token"}
 
 friends = requests.get("https://discord.com/api/v9/users/@me/relationships", headers = token)
 
